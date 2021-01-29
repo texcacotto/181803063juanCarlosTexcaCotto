e52d033f5b497cc84ffde12f58ccd67fd12131ec
texcacotto <texcacotto@gmail.com>
2021-01-29 09:14:21 -0600

    update

--- Casting/src/main/java/Explicito.java
+++ Casting/src/main/java/Explicito.java
@@ -8,12 +8,10 @@
  *
  * @author charl
  */
-public class ConvertirString {
+public class Explicito {
     public static void main(String[] args) {
-       String numero = "1";
-       int numeroReal = Integer.parseInt(numero);
-       int resultado = numeroReal + 5;
-       String resultadoFinal = "El resultado final es: " + resultado;
-       System.out.println(resultadoFinal);
+        double valorMayor = 8;
+        int valorMenor = (int) valorMayor;
+        System.out.println(valorMenor);
     }
 }
