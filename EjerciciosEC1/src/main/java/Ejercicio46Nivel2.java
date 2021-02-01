a2e57cb812821984ab2f4ea2c61197d661aa4f92
texcacotto <texcacotto@gmail.com>
2021-01-31 18:12:48 -0600

    update

--- EjerciciosEC1/src/main/java/Ejercicio46Nivel2.java
+++ EjerciciosEC1/src/main/java/Ejercicio46Nivel2.java
@@ -1,3 +1,7 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -7,6 +11,7 @@
 /*
 Un programa que solicite 3 números,
 y los imprima ordenados de menor a mayor.
+https://github.com/texcacotto/181803063juanCarlosTexcaCotto/blob/master/EjerciciosEC1/src/main/java/Ejercicio46Nivel2.java
 */
 
 /**
@@ -14,5 +19,15 @@ y los imprima ordenados de menor a mayor.
  * @author charl
  */
 public class Ejercicio46Nivel2 {
-    
+    public static void main(String[] args) {
+        int [] arreglo = new int[3];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingresa 3 números:");
+        for(int i = 0; i < arreglo.length; i++) {
+            arreglo[i] = scanner.nextInt();
+        }
+        scanner.close();
+        Arrays.sort(arreglo);
+        System.out.println("Resultado:\n" + Arrays.toString(arreglo));
+    }
 }
