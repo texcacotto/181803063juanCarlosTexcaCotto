6427ead2b20d1f3520023fbfc604474e44170b72
texcacotto <texcacotto@gmail.com>
2021-02-06 14:36:59 -0600

    update

--- ModuloMaterias/src/test/java/TestMateria.java
+++ ModuloMaterias/src/test/java/TestMateria.java
@@ -18,16 +18,26 @@ https://github.com/texcacotto/181803063juanCarlosTexcaCotto/blob/master/ModuloMa
 public class TestMateria {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        //System.out.println("Ingresa el nombre de la materia:");
-        //String nombreMateria = scanner.nextLine();
-        
-        
-        
-        Materia materiaUno = new Materia("Redes", "TI", 10, "redesTI2021", "Enero - Abril 2021");
-        Materia materiaDos = new Materia(nombreMateria, carrera, 0, codigoMateria, cuatrimestre);
-        Materia materiaTres = new Materia();
+        System.out.println("\n¡Ingresa una nueva materia!");
+        System.out.println("Nombre:");
+        String nombreMateria = scanner.nextLine();
+        System.out.println("Carrera:");
+        String carrera = scanner.nextLine();
+        System.out.println("Créditos:");
+        int creditos = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Código:");
+        String codigoMateria = scanner.nextLine();
+        System.out.println("Cuatrimestre:");
+        String cuatrimestre = scanner.nextLine();
+        System.out.println("¡Materia guardada!");
+        Materia materiaUno = new Materia("POO", "TI", 20, "ITI-POO", "Enero-Abril 2021");
+        Materia materiaDos = new Materia("BD", "TI", 15, "ITI-BD", "Enero-Abril 2021");
+        Materia materiaTres = new Materia("Redes", "TI", 10, "ITI-R", "Enero-Abril 2021");
+        Materia materiaCuatro = new Materia(nombreMateria, carrera, creditos, codigoMateria, cuatrimestre);
         System.out.println(materiaUno);
         System.out.println(materiaDos);
         System.out.println(materiaTres);
+        System.out.println(materiaCuatro);
     }
 }
