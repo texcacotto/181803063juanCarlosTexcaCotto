9715c7ed2e663df185c83dd26ee86a215ac54f08
texcacotto <cotto.degallio@gmail.com>
2021-03-06 01:30:07 -0600

    update

--- Usuarios/src/main/java/Usuario.java
+++ Usuarios/src/main/java/Usuario.java
@@ -10,15 +10,15 @@
  */
 public class Usuario {
     
-    private int id, status;
+    private int idUsuario, status;
     //Para status: 1 - Vigente / 0 - No vigente
-    private String nombreUsuario, contraseña, fechaAlta;
+    private String nombre, password, fechaAlta;
 
-    public Usuario(int id, int status, String nombreUsuario, String contraseña, String fechaAlta) {
-        this.id = id;
+    public Usuario(int idUsuario, int status, String nombre, String password, String fechaAlta) {
+        this.idUsuario = idUsuario;
         this.status = status;
-        this.nombreUsuario = nombreUsuario;
-        this.contraseña = contraseña;
+        this.nombre = nombre;
+        this.password = password;
         this.fechaAlta = fechaAlta;
     }
 
@@ -27,36 +27,36 @@ public class Usuario {
 
     @Override
     public String toString() {
-        return "Usuario{" + "id=" + id + ", status=" + status + ", nombreUsuario=" + nombreUsuario + ", contrase\u00f1a=" + contraseña + ", fechaAlta=" + fechaAlta + '}';
+        return "Usuario{" + "idUsuario=" + idUsuario + ", status=" + status + ", nombre=" + nombre + ", password=" + password + ", fechaAlta=" + fechaAlta + '}';
     }
 
-    public int getId() {
-        return id;
+    public int getIdUsuario() {
+        return idUsuario;
     }
     public int getStatus() {
         return status;
     }
-    public String getNombreUsuario() {
-        return nombreUsuario;
+    public String getNombre() {
+        return nombre;
     }
-    public String getContraseña() {
-        return contraseña;
+    public String getPassword() {
+        return password;
     }
     public String getFechaAlta() {
         return fechaAlta;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
     }
     public void setStatus(int status) {
         this.status = status;
     }
-    public void setNombreUsuario(String nombreUsuario) {
-        this.nombreUsuario = nombreUsuario;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
-    public void setContraseña(String contraseña) {
-        this.contraseña = contraseña;
+    public void setPassword(String password) {
+        this.password = password;
     }
     public void setFechaAlta(String fechaAlta) {
         this.fechaAlta = fechaAlta;
