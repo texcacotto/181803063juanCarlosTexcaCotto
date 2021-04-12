9720123a7e152fd962b3cfcab43326d1df8e0a93
texcacotto <cotto.degallio@gmail.com>
2021-04-12 00:45:20 -0500

    update

--- LicenciasFederales/src/main/java/Vista/Index.java
+++ LicenciasFederales/src/main/java/Vista/Index.java
@@ -51,7 +51,7 @@ public class Index extends javax.swing.JFrame {
         jButton2.setBackground(new java.awt.Color(51, 51, 51));
         jButton2.setFont(new java.awt.Font("Dialog", 1, 11)); // NOI18N
         jButton2.setForeground(new java.awt.Color(255, 255, 255));
-        jButton2.setText("Tipos de Vehículos");
+        jButton2.setText("Tipos de vehículos");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton2ActionPerformed(evt);
